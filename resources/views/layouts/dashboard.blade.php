1bf08fc4c7bb09cb13123fa723ca54dccf2c0887
GreenWhite <greenwhitedev@gmail.com>
2018-11-01 04:25:50 +0100

    Added css and js assets

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -14,20 +14,13 @@
 
     <title>@yield('title') | {{config('app.name')}}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
     <!-- Styles -->
-    <link rel="shortcut icon" href="{{asset($public.'/dashboard/png/favicon.png')}}">
+    <link rel="shortcut icon" href="{{asset($public.'/png/favicon.png')}}">
     <link rel="icon" type="image/png" sizes="192x192" href="{{asset($public.'/png/favicon-192x192.png')}}">
     <link rel="apple-touch-icon" sizes="180x180"
-          href="{{asset($public.'/dashboard/png/apple-touch-icon-180x180.png')}}">
-    <link rel="stylesheet" id="css-main" href="{{asset($public.'/dashboard/css/codebase.min-1.4.css')}}">
-    <link rel="stylesheet" id="css-main" href="{{asset($public.'/dashboard/css/datatables.min.css')}}">
+          href="{{asset($public.'/png/apple-touch-icon-180x180.png')}}">
+    <link rel="stylesheet" id="css-main" href="{{asset($public.'/css/codebase.min-1.4.css')}}">
+    <link rel="stylesheet" id="css-main" href="{{asset($public.'/css/datatables.min.css')}}">
     <link rel="stylesheet" href="{{asset($public.'/css/sweetalert.min.css')}}">
     @yield('styles')
 </head>
@@ -40,9 +33,9 @@
     </main>
     @include('layouts.partials.footer')
 </div>
-<script src="{{asset($public.'/dashboard/js/codebase.min-1.4.js')}}"></script>
-<script src="{{asset($public.'/dashboard/js/be_pages_dashboard.js')}}"></script>
-<script src="{{asset($public.'/dashboard/js/datatables.min.js')}}"></script>
+<script src="{{asset($public.'/js/codebase.min-1.4.js')}}"></script>
+<script src="{{asset($public.'/js/be_pages_dashboard.js')}}"></script>
+<script src="{{asset($public.'/js/datatables.min.js')}}"></script>
 <script src="{{asset($public.'/js/sweetalert.min.js')}}"></script>
 <script>
     $.ajaxSetup({
